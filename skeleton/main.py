8b8392c1bfdc87c94b44b1989d47fbe2603e363f
Mark Quercioli <mark@Marks-MacBook-Pro-2.local>
2023-01-07 18:22:31 -0500

    landing page

--- skeleton/main.py
+++ skeleton/main.py
@@ -8,28 +8,32 @@ TOKEN = "figd_ASdlJUjKD8bjctapoAekHKm8MbucdvOTKNhx7Xbu"
 load_dotenv()
 
 app = Flask(__name__)
-# url = os.getenv("DATABASE_URL")
-conn = psycopg2.connect("dbname='postgres'  user='postgres' password='' host='localhost' port='5432'")
+url = os.getenv("DATABASE_URL")
+conn = psycopg2.connect(url)
 # Create a cursor
-cursor = conn.cursor()
+# cursor = conn.cursor()
+#
+# # Execute a query
+# cursor.execute("SELECT * FROM users")
+#
+# # Fetch the results
+# results = cursor.fetchall()
+#
+# # Loop through the results
+# for result in results:
+#     print(result)
 
-# Execute a query
-cursor.execute("SELECT * FROM users")
+CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS user (id SERIAL PRIMARY KEY, first TEXT, last TEXT , phone INTEGER, email TEXT, capital REAL) """
 
-# Fetch the results
-results = cursor.fetchall()
-
-# Loop through the results
-for result in results:
-    print(result)
-
-
+INSERT_NEW_USER = "INSERT INTO user (first, last, phone, email, capital) VALUES (%s,%s,%s,%s,%s) RETURNING id"
 
 @app.route('/', methods=['GET', 'POST'])
 def landing():
     return render_template("opt-in.html")
 
-
+    data = request.get_json()
+    first = data['first']
+    print(first)
 @app.route('/create-acct', methods=['GET', 'POST'])
 def create_acct():
     return render_template("create-account.html")
